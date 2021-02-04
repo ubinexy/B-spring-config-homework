3aa677de4736969946ab2695265d6fca05440d49
shiqi <shiqi.tong@thoughtworks.com>
2021-02-05 00:47:04 +0800

    p2s3: DynamicPropertySource annotation

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -16,17 +18,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestPropertySource(properties = {"levelNumber=1"})
 public class LevelControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
-//    @Test
-//    public void should_return_level_basic_when_Level_number_is_less_than_one() throws Exception {
-//        MvcResult mvcResult = mvc.perform(get("/level")).andExpect(status().isOk()).andReturn();
-//        assertEquals(mvcResult.getResponse().getContentAsString(), "basic");
-//    }
+
+    @DynamicPropertySource
+    static void registerPgProperties(DynamicPropertyRegistry registry) {
+        registry.add("levelNumber", () -> 2);
+    }
 
     @Test
     public void should_return_level_basic_when_Level_number_is_greater_equal_than_one() throws Exception {
